43eb6650b48379c63e26f6d31f4f4febfe39e094
KAWINDI132004 <kawindimuhandiram@gmail.com>
2025-05-03 17:18:55 +0530

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,8 +1,8 @@
 import random
 
 while True:
-    number = random.randint(1, 100)
-    print("Guess a number between 1 and 100")
+    number = random.randint(1, 50)
+    print("Guess a number between 1 and 50")
     guess = int(input())
 
     if guess < number:
